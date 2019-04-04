90edbe33c51ff3bf81bcf6931ad507ab90db8053
Alex Lakatos <alex.lakatos.qa@gmail.com>
2019-04-04 13:53:04 +0100

    add extended IVR menu

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 
+const request = require('request')
+
 app.use(bodyParser.json())
 
 app.get('/webhooks/answer', (req, res) => {
   const ncco = [{
       action: 'talk',
+      voiceName: 'Chipmunk',
       bargeIn: true,
-      text: 'Hello. Please enter a digit.'
+      text: '<speak>Hello. Please enter a number between 0 and <prosody rate="fast">99999999999999999999</prosody> followed by the # key.</speak>'
     },
     {
       action: 'input',
-      maxDigits: 1,
+      maxDigits: 20,
+      timeOut: 10,
+      submitOnHash: true,
       eventUrl: [`${req.protocol}://${req.get('host')}/webhooks/dtmf`]
     }
   ]
@@ -25,12 +30,33 @@ app.post('/webhooks/events', (req, res) => {
 })
 
 app.post('/webhooks/dtmf', (req, res) => {
-  const ncco = [{
-    action: 'talk',
-    text: `You pressed ${req.body.dtmf}`
-  }]
+  let number = req.body.dtmf || 42;
+  let message = "";
 
-  res.json(ncco)
+  request(`http://numbersapi.com/${number}`, (error, response, body) => {
+    if (error) {
+      message = "The Numbers API has thrown an error."
+    } else {
+      message = body
+    }
+
+    const ncco = [{
+        action: 'talk',
+        bargeIn: true,
+        voiceName: 'Chipmunk',
+        text: `<speak><s>${message}</s> <s>Enter another number if you want to continue or just hang up the call if you've had enough.</s></speak>`
+      },
+      {
+        action: 'input',
+        maxDigits: 20,
+        timeOut: 10,
+        submitOnHash: true,
+        eventUrl: [`${req.protocol}://${req.get('host')}/webhooks/dtmf`]
+      }
+    ]
+
+    res.json(ncco)
+  })
 })
 
 app.listen(3000)
